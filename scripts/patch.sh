4c4a62ff549872813ddc1d394324ba5dba3941c1
Alex Deiter <alex.deiter@nexenta.com>
2018-02-19 12:55:45 +0000

    Added logs for patch/test

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -11,9 +11,12 @@ unset PS4
 typeset _pwd=$(pwd)
 typeset _project=$(basename $_pwd)
 typeset _script=$(readlink -f $0)
+typeset _ident=$(basename -s .sh $_script)
 typeset _source=$(dirname $_script)
 
 source $_source/env.sh
+
+exec > >(tee -a $_base/logs/$_ident.log | logger -t $_ident) 2>&1
 
 if [[ -f $_base/$_ci/patches/$_project/$_branch.diff ]]; then
     patch -p1 < $_base/$_ci/patches/$_project/$_branch.diff
--- scripts/test.sh
+++ scripts/test.sh
@@ -9,10 +9,13 @@ set -o noglob
 typeset _pwd=$(pwd)
 typeset _project=$(basename $_pwd)
 typeset _script=$(readlink -f $0)
+typeset _ident=$(basename -s .sh $_script)
 typeset _source=$(dirname $_script)
 typeset _error
 
 source $_source/env.sh
+
+exec > >(tee -a $_base/logs/$_ident.log | logger -t $_ident) 2>&1
 
 cd $_base/tempest
 set +o errexit
