8f881195522d03639a4f44c5620cb234ee610a3d
Alex Deiter <alex.deiter@nexenta.com>
2018-03-30 23:11:34 +0000

    Added tempest retries option

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,8 +13,13 @@ typeset _ident=$(basename -s .sh $_script)
 typeset _source=$(dirname $_script)
 
 source $_source/env.sh
-
 exec > >(tee -a $_base/$_logs/$_ident.log | logger -t $_ident) 2>&1
-
 cd $_base/tempest
-tox -e ci -- $_regexp
+
+for i in $(seq $_retries); do
+	if tox -e ci -- $_regexp; then
+		exit 0
+	fi
+done
+
+exit 1
