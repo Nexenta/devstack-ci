fc6315019ff4b9fdb0e4ed4e2e49877be8815804
Alex Deiter <alex.deiter@gmail.com>
2020-09-30 19:17:45 +0300

    Update env

---
+++ scripts/stack.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o noglob
+
+typeset _pwd=$(pwd)
+typeset _project=$(basename $_pwd)
+typeset _script=$(readlink -f $0)
+typeset _ident=$(basename -s .sh $_script)
+typeset _source=$(dirname $_script)
+typeset _unit _log
+
+source $_source/env.sh
+
+export UPPER_CONSTRAINTS_FILE="$_base/requirements/upper-constraints.txt"
+
+exec $_base/devstack/stack.sh
